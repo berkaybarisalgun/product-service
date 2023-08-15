b1c43bc8c4444a107f906ee300a3fd2c96436c9f
Berkay Baris Algun <bbarisalgun@gmail.com>
2023-08-15 20:07:24 +0300

    Added requirements

--- src/test/java/com/berkaybarisalgun/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/berkaybarisalgun/productservice/ProductServiceApplicationTests.java
@@ -2,12 +2,30 @@ package com.berkaybarisalgun.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Container
+	static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri",mongoDBContainer::getReplicaSetUrl);
 	}
+
+
+	@Test
+	void shouldCreateProduct() {
+
+	}
+
 
 }
